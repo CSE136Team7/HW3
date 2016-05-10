3a5a2094de398c7552abfc01f09ff8e2de519595
bhavikup <Bhavikup@gmail.com>
2016-05-10 16:39:51 -0700

    setup

---
+++ config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  DATABASE_HOST: 'localhost',
+  DATABASE_USER: 'root',
+  DATABASE_PASSWORD: '',
+  DATABASE_NAME: 'books',
+  PORT: 8000,
+  USERNAME: 'cse136User',
+  PASSWORD: 'cse136PW',
+};
