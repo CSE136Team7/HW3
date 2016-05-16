eea5bb120927168d6fa1f69d2d397853c4144c48
crousseaux <constancerousseaux@gmail.com>
2016-05-16 01:27:20 -0700

    signup

--- users.js
+++ users.js
@@ -2,14 +2,14 @@
  Values are hard coded for example purposes
  */
 
-var config = require('./config');
-var db = require('./db');
+ var config = require('./config');
+ var db = require('./db');
 
 /**
  *
  * Attempt to login the user.  Redirect to /books on successful login and /login on unsuccessful attempt.
  */
-module.exports.login = function(req, res) {
+ module.exports.login = function(req, res) {
   if (req.body.username === config.USERNAME && req.body.password === config.PASSWORD) {
     req.session.user = req.body.username;
     // Needs to be changed to be specific to the user that is logged in
@@ -24,7 +24,7 @@ module.exports.login = function(req, res) {
 /**
  * Render the login form
  */
-module.exports.loginForm = function(req, res){
+ module.exports.loginForm = function(req, res){
   res.render('users/login');
 };
 
@@ -33,15 +33,15 @@ module.exports.loginForm = function(req, res){
 /**
  * Clear out the session to logout the user
  */
-module.exports.logout = function(req, res) {
-    req.session.destroy();
-    res.redirect('/login');
+ module.exports.logout = function(req, res) {
+  req.session.destroy();
+  res.redirect('/login');
 };
 
 /**
  * Verify a user is logged in.  This middleware will be called before every request to the books directory.
  */
-module.exports.auth = function(req, res, next) {
+ module.exports.auth = function(req, res, next) {
   if (req.session && req.session.user === config.USERNAME) {
     return next();
   }
@@ -61,7 +61,7 @@ module.exports.auth = function(req, res, next) {
 
 
 module.exports.newAccount = function(req, res){
-  
+
   if (req.body.username != "" && req.body.password != "" ){
     var user = req.body.username;
     var pwd = req.body.password;
@@ -76,16 +76,14 @@ module.exports.newAccount = function(req, res){
     //       if (results.length==0){
             //no existing username --> insert into the table
                //hashing of the password   
-              var queryString = 'INSERT INTO users(username, passhash) VALUES (' + user +','+ pwd  + ')';
-              db.query(queryString, function(err, result){ 
-                if (err)
-                    throw err;
-                     });
-  //               else{
-  //                   //render an alert message : the account have been created
-  //                   res.render('users/success');
-  //               }
-  //             });
+               var queryString = "INSERT INTO users(username, passhash) VALUES ("+ db.escape(user) + "," + db.escape(pwd) +")";
+               db.query(queryString, function(err, result){ 
+                if (err) throw err;  
+                else{
+                    //render an alert message : the account have been created
+                    res.render('users/success');
+                  }}
+                  );
   //         }
   //         else{
   //           //already existing username --> alert message
@@ -96,5 +94,5 @@ module.exports.newAccount = function(req, res){
   // }
   // else{
   //   res.render('users/errorBadForm');
-  }
- };
+}
+};
