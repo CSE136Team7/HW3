f0ea7ce07fabe7fc4568120f2d645459ed16127d
Bal Krishna Oli <bkoli@ucsd.edu>
2016-05-17 08:25:28 -0700

    chage config file for server

--- config.js
+++ config.js
@@ -1,7 +1,14 @@
 module.exports = {
-  DATABASE_HOST: '104.131.110.183',
+  DATABASE_HOST: 'localhost',
   DATABASE_USER: 'root',
-  DATABASE_PASSWORD: 'Vh9iPZwqSU',
+  DATABASE_PASSWORD: '',
   DATABASE_NAME: 'bookmarks',
-  PORT: 8080,
+  PORT: 8000,
 };
+/*
+DATABASE_HOST: '104.131.110.183',
+DATABASE_USER: 'root',
+DATABASE_PASSWORD: 'Vh9iPZwqSU',
+DATABASE_NAME: 'bookmarks',
+PORT: 8080,
+*/
