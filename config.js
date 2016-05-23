cc50c0933201b98ceeddfcf4d9959890066a70c0
Brian Lam <btlam@ucsd.edu>
2016-05-22 20:09:59 -0700

    changed config back for master

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 module.exports = {
-  DATABASE_HOST: '104.131.110.183',
+  DATABASE_HOST: 'localhost',
   DATABASE_USER: 'root',
-  DATABASE_PASSWORD: 'Vh9iPZwqSU',
+  DATABASE_PASSWORD: '',
   DATABASE_NAME: 'bookmarks',
-  PORT: process.env.PORT||8080,
+  PORT: 8000,
   SECRET: 'T34m7R0ckx'
 };
 /*
