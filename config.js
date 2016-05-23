a47f5c831676cae337ff291382b705f86ed6f1e8
Bal Krishna Oli <bkoli@ucsd.edu>
2016-05-22 19:54:47 -0700

    update config

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 module.exports = {
-  DATABASE_HOST: 'localhost',
+  DATABASE_HOST: '104.131.110.183',
   DATABASE_USER: 'root',
-  DATABASE_PASSWORD: '',
+  DATABASE_PASSWORD: 'Vh9iPZwqSU',
   DATABASE_NAME: 'bookmarks',
-  PORT: 8000,
+  PORT: process.env.PORT||8080,
   SECRET: 'T34m7R0ckx'
 };
 /*
