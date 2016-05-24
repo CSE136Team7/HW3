2920ad2c6c47f2485e6f20f2c379cef5f50d6eef
Bal Krishna Oli <bkoli@ucsd.edu>
2016-05-23 22:20:07 -0700

    updated db.js

--- db.js
+++ db.js
@@ -6,7 +6,13 @@ var MySQL = function() {
 
     return {
         init: function(){
-            MySQL.connection = mysql.createConnection('mysql://bf10eaf95350d6:d8fcd7ae@us-cdbr-iron-east-04.cleardb.net/heroku_3ddd6cbd9fad3cb?reconnect=true');
+            MySQL.connection = mysql.createConnection({
+                host     : config.DATABASE_HOST,
+                user     : config.DATABASE_USER,
+                password : config.DATABASE_PASSWORD,
+                database : config.DATABASE_NAME
+            });
+
             MySQL.connection.connect();
         },
         query: function(querystring, callback){
@@ -17,37 +23,3 @@ var MySQL = function() {
 }();
 
 module.exports = MySQL;
-
-// var mysql      = require('mysql');
-// var config = require('./config');
-//
-// var MySQL = function() {
-//     var connection;
-//
-//     return {
-//         init: function(){
-//             MySQL.connection = mysql.createConnection('mysql://bf10eaf95350d6:d8fcd7ae@us-cdbr-iron-east-04.cleardb.net/heroku_3ddd6cbd9fad3cb?reconnect=true');
-//             MySQL.connection.connect(function(err) {              // The server is either down
-//                 if(err) {                                     // or restarting (takes a while sometimes).
-//                   console.log('error when connecting to db:', err);
-//                   setTimeout(MySQL, 2000); // We introduce a delay before attempting to reconnect,
-//                 }                                     // to avoid a hot loop, and to allow our node script to
-//               });
-//
-//               MySQL.connection.on('error', function(err) {
-//                 console.log('db error', err);
-//                 if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
-//                    MySQL.init();                          // lost due to either server restart, or a
-//                 } else {                                      // connnection idle timeout (the wait_timeout
-//                   throw err;                                  // server variable configures this)
-//                 }
-//               });
-//         },
-//         query: function(querystring, callback){
-//             MySQL.connection.query(querystring, callback);
-//         },
-//         escape: mysql.escape
-//     }
-// }();
-//
-// module.exports = MySQL;
