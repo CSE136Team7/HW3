7f608c674493ac566e1dd1cd05b704023d4f41ac
bhavikup <Bhavikup@gmail.com>
2016-06-02 18:22:30 -0700

    changing back

--- config.js
+++ config.js
@@ -1,11 +1,15 @@
 module.exports = {
-  DATABASE_HOST: '104.131.110.183',
+  DATABASE_HOST: 'localhost',
   DATABASE_USER: 'root',
-  DATABASE_PASSWORD: 'Vh9iPZwqSU',
+  DATABASE_PASSWORD: '',
   DATABASE_NAME: 'bookmarks',
-  PORT: 8080,
+  PORT: process.env.PORT || 8000,
   SECRET: 'T34m7R0ckx'
 };
 /*
-
+DATABASE_HOST: '104.131.110.183',
+DATABASE_USER: 'root',
+DATABASE_PASSWORD: 'Vh9iPZwqSU',
+DATABASE_NAME: 'bookmarks',
+PORT: 8080,
 */
